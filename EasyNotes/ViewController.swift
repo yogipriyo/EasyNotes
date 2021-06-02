148fe017432e6ae0da597ffcb9321516ddf621b5
Yogi Priyo Prayogo <yogipriyo.prayogo@gmail.com>
2021-06-03 00:09:53 +0700

    initial start

---
+++ EasyNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyNotes
+//
+//  Created by Yogi Priyo Prayogo on 02/06/21.
+//  Copyright © 2021 Yogi Priyo Prayogo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
